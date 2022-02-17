74413ac2504fe252cf31bd895436100efb1c688f
JJS4M <sam_jj72@yahoo.com>
2022-02-18 00:22:10 +0800

    gitignore

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
-set PATH=%PATH%;YOUR_PATH
-set COB_CONFIG_DIR=YOUR_PATH
-set COB_COPY_DIR=YOUR_PATH
-set COB_INCLUDE_PATH=YOUR_PATH
-set COB_LIB_PATH=YOUR_PATH
+set PATH=%PATH%;D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\bin
+set COB_CONFIG_DIR=D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\config
+set COB_COPY_DIR=D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\copy
+set COB_INCLUDE_PATH=D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\include
+set COB_LIB_PATH=D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\lib
 
 mingw32-make
