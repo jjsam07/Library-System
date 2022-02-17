5ae8c72c33153e086a877ee44e375334c9739303
francisemmanuel19 <francisemmanuelyandoc@gmail.com>
2022-02-18 00:29:35 +0800

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,7 +1,15 @@
-set PATH=%PATH%;YOUR_PATH
-set COB_CONFIG_DIR=YOUR_PATH
-set COB_COPY_DIR=YOUR_PATH
-set COB_INCLUDE_PATH=YOUR_PATH
-set COB_LIB_PATH=YOUR_PATH
+<<<<<<< HEAD
+set PATH=%PATH%;C:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\bin
+set COB_CONFIG_DIR=C:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\config
+set COB_COPY_DIR=C:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\copy
+set COB_INCLUDE_PATH=C:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\include
+set COB_LIB_PATH=C:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\lib
+=======
+set PATH=%PATH%;D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\bin
+set COB_CONFIG_DIR=D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\config
+set COB_COPY_DIR=D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\copy
+set COB_INCLUDE_PATH=D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\include
+set COB_LIB_PATH=D:\Program Files (x86)\OpenCobolIDE\GnuCOBOL\lib
+>>>>>>> 74413ac2504fe252cf31bd895436100efb1c688f
 
 mingw32-make
